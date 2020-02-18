298d0fb41ab806b4891979978280548789f7aaa8
PamelaCarreira <57276096+PamelaCarreira@users.noreply.github.com>
2020-02-17 21:53:10 -0300

    Add files via upload

---
+++ php/exercicio2.php
@@ -0,0 +1,8 @@
+<?php
+    for ($num = 0; $num <= 10; $num++){
+    echo $num.'<br>';
+        if ($num == 5){
+            break;
+        }
+    }
+?>
---
+++ php/exercicio3.php
@@ -0,0 +1,11 @@
+<?php
+    $num=1;
+    switch ($num) {
+        case 0:
+            echo "Opção 1";
+            break;
+        case 1:
+            echo "Opção 2";
+            break;
+    }
+?>
